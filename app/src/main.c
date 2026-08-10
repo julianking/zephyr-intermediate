383fc9b1dcf1078762156edbd799e5e0b7079477
Bogumił Zarzycki <b.zarzycki@o2.pl>
2026-08-10 20:01:06 +0200

    Basic thread

--- app/src/main.c
+++ app/src/main.c
@@ -5,27 +5,67 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
 #define STACK_SIZE 1024
 
-#define PRIO_A 5
-#define PRIO_B 5
+#define PRIO_LOW    7
+#define PRIO_MED    5
+#define PRIO_HIGH   3
+#define PRIO_COOP   (-1)
 
-void thread_a_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        k_msleep(200);
-    }
-}
 
-void thread_b_fn(void *p1, void *p2, void *p3)
+void low_fn(void *p1, void *p2, void *p3)
 {
+    LOG_INF("[LOW] starting");
+    
     while (1) {
+        LOG_INF("[LOW] loop");
         k_msleep(300);
     }
 }
 
-K_THREAD_DEFINE(thread_a, STACK_SIZE, thread_a_fn,
-                NULL, NULL, NULL, PRIO_A, 0, 0);
-K_THREAD_DEFINE(thread_b, STACK_SIZE, thread_b_fn,
-                NULL, NULL, NULL, PRIO_B, 0, 0);
+void med_fn(void *p1, void *p2, void *p3)
+{
+    LOG_INF("[MED] starting");
+
+    while (1) {
+        LOG_INF("[MED] loop");
+        k_msleep(200);
+    }
+}
+
+
+void high_fn(void *p1, void *p2, void *p3)
+{
+    LOG_INF("[HIGH] starting");
+
+    while (1) {
+        LOG_INF("[HIGH] loop");
+        k_msleep(100);
+    }
+}
+
+void coop_fn(void *p1, void *p2, void *p3)
+{
+    LOG_INF("[COOP] starting");
+
+    for (uint16_t i=1; i<=5; i++) {
+        LOG_INF("[COOP] iteration %d", i);
+        k_busy_wait(20000);
+    }
+
+    LOG_INF("[COOP] yielding...");
+    k_yield();
+
+    LOG_INF("[COOP] work done!");
+}
+
+K_THREAD_DEFINE(t_high_fn, STACK_SIZE, high_fn,
+                NULL, NULL, NULL, PRIO_HIGH, 0, 0);
+K_THREAD_DEFINE(t_med_fn, STACK_SIZE, med_fn,
+                NULL, NULL, NULL, PRIO_MED, 0, 0);
+K_THREAD_DEFINE(t_low_fn, STACK_SIZE, low_fn,
+                NULL, NULL, NULL, PRIO_LOW, 0, 0);
+K_THREAD_DEFINE(t_coop_fn, STACK_SIZE, coop_fn,
+                NULL, NULL, NULL, PRIO_COOP, 0, 0);
+
 
 int main(void)
 {
