4c414ffdbbcb8540dd32a80cf9b5d0bd1b299cc8
Bogumił Zarzycki <b.zarzycki@o2.pl>
2026-08-18 20:04:09 +0200

    Semaphores and Mutexes

--- app/src/main.c
+++ app/src/main.c
@@ -5,70 +5,54 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
 #define STACK_SIZE 1024
 
-#define PRIO_LOW    7
-#define PRIO_MED    5
-#define PRIO_HIGH   3
-#define PRIO_COOP   (-1)
+#define PRIO_A      5
+#define PRIO_B      5
+#define INCREMENTS  1000000
 
+static struct k_sem done_sem;
+static struct k_mutex counter_mutex;
+static volatile uint32_t counter=0;
 
-void low_fn(void *p1, void *p2, void *p3)
-{
-    LOG_INF("[LOW] starting");
-    
-    while (1) {
-        LOG_INF("[LOW] loop");
-        k_msleep(300);
+void worker_fn(void *p1, void *p2, void *p3)
+{   
+    const char *name = k_thread_name_get(k_current_get());
+
+    for (int i=0; i < INCREMENTS; i++) {
+        k_mutex_lock(&counter_mutex, K_FOREVER);
+        counter++;
+        k_mutex_unlock(&counter_mutex);
     }
-}
 
-void med_fn(void *p1, void *p2, void *p3)
-{
-    LOG_INF("[MED] starting");
-
-    while (1) {
-        LOG_INF("[MED] loop");
-        k_msleep(200);
-    }
+    LOG_INF("[%s] finished its work", name);
+    k_sem_give(&done_sem);
 }
 
 
-void high_fn(void *p1, void *p2, void *p3)
-{
-    LOG_INF("[HIGH] starting");
-
-    while (1) {
-        LOG_INF("[HIGH] loop");
-        k_msleep(100);
-    }
-}
-
-void coop_fn(void *p1, void *p2, void *p3)
-{
-    LOG_INF("[COOP] starting");
-
-    for (uint16_t i=1; i<=5; i++) {
-        LOG_INF("[COOP] iteration %d", i);
-        k_busy_wait(20000);
-    }
-
-    LOG_INF("[COOP] yielding...");
-    k_yield();
-
-    LOG_INF("[COOP] work done!");
-}
-
-K_THREAD_DEFINE(t_high_fn, STACK_SIZE, high_fn,
-                NULL, NULL, NULL, PRIO_HIGH, 0, 0);
-K_THREAD_DEFINE(t_med_fn, STACK_SIZE, med_fn,
-                NULL, NULL, NULL, PRIO_MED, 0, 0);
-K_THREAD_DEFINE(t_low_fn, STACK_SIZE, low_fn,
-                NULL, NULL, NULL, PRIO_LOW, 0, 0);
-K_THREAD_DEFINE(t_coop_fn, STACK_SIZE, coop_fn,
-                NULL, NULL, NULL, PRIO_COOP, 0, 0);
-
+K_THREAD_DEFINE(thread_a, STACK_SIZE, worker_fn,
+                NULL, NULL, NULL, PRIO_A, 0, 0);
+K_THREAD_DEFINE(thread_b, STACK_SIZE, worker_fn,
+                NULL, NULL, NULL, PRIO_B, 0, 0);
 
 int main(void)
 {
+    k_mutex_init(&counter_mutex);
+    k_sem_init(&done_sem, 0, 2);
+
+    LOG_INF("Expected final counter value: %u", INCREMENTS*2);
+
+    k_sem_take(&done_sem, K_FOREVER);
+    k_sem_take(&done_sem, K_FOREVER);
+
+    LOG_INF("Actual final counter value: %u", counter);
+
+    if (counter == INCREMENTS*2) {
+        LOG_WRN("No race this run!");
+    }
+    else {
+        LOG_ERR("Race condition detected! Lost %u updates!", 
+            INCREMENTS*2 - counter);
+    }
+
     return 0;
 }
 
